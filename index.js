6d208a25a6835110d1aeaa96fd0c466f91ce166f
BacNguyen <luongbac301092@gmail.com>
2020-04-03 10:47:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const jping = require('jjg-ping');
 const bot = new Discord.Client();
-const token = 'Njk1Mjc0MzY3MzIzNDcxODgz.XoX3tQ.5kY09_q7FeLG4BVdV-C87PRCY-4';
+const token = process.env.TOKEN;
 
 const defaultServers  = {"US (East)":["dynamodb.us-east-1.amazonaws.com","Sterling",true],"US (West)":["dynamodb.us-west-1.amazonaws.com","California",true],"Europe":["dynamodb.eu-central-1.amazonaws.com","Central",true],"Asia (SEA)":["dynamodb.ap-southeast-1.amazonaws.com","Singapore",true],"South America":["dynamodb.sa-east-1.amazonaws.com","Brazil",true],"OCE":["dynamodb.ap-southeast-2.amazonaws.com","Australia Sydney",true],"China":["hkt.com","Hong Kong",true],"Asia":["dynamodb.ap-northeast-1.amazonaws.com","Japan Tokyo",true],"EU West":["dynamodb.eu-west-2.amazonaws.com","Luxemburg",true]};
 
